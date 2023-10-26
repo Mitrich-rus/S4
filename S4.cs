c29b95c0d953ccea5edf3f241faaff5a8f161d77
Mitrich-rus <146307143+Mitrich-rus@users.noreply.github.com>
2023-10-27 02:16:39 +1000

    s4

---
+++ S4.cs
@@ -0,0 +1,77 @@
+﻿// Задача 24: Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// тип возвращающего значения + Название + () + {}
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int GetSum()
+// {
+//     int start = 1;
+//     int sum=0;
+
+//     while(start<= num)
+//     {
+//         sum += start;
+//      // sum = sum +start;
+//         start++;
+//     }
+//     return sum;
+// }
+
+// // int result= GetSum();
+// // System.Console.WriteLine(result);
+
+// System.Console.WriteLine(GetSum());
+
+// Задача 28: Напишите программу, которая принимает
+//  на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int GetSum()
+// {
+//     int start = 1;
+//     int multiplication = 1;
+
+//     while(start<= num)
+//     {
+//         multiplication *= start;
+//      // Multiplication = Multiplication +start;
+//         start++;
+
+//     }
+//     return multiplication;
+// }
+
+// // int result= GetSum();
+// // System.Console.WriteLine(result);
+
+// Console.WriteLine(GetSum());
+
+
+// Задача 30: Напишите программу, которая выводит 
+// массив из 8 элементов, заполненный нулями и 
+// единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// CreateArray();
+
+// void CreateArray()
+// {
+//     int[] array = new int[8];
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0,2);
+//         System.Console.Write(array[i]+ " "); 
+//     }
+// }
+
